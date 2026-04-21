49bb06c68a5c62b5e7347a6f91218eb3a3ca1151
facundobatle19-svg <facundobatle19@gmail.com>
2026-04-21 16:26:56 -0300

    Update jm2 server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function getPuppeteerConfig() {
   if (isRender) {
     return {
       headless: true,
-      executablePath: puppeteer.executablePath(), // 🔥 CLAVE
+      executablePath: "/usr/bin/chromium-browser", // 🔥 USAR EL DEL SISTEMA
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
